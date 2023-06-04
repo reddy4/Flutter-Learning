d2afb494d9900279c99918cf16ef9efe58467a2f
Reddanna Kotte <reddy.iphonedev@gmail.com>
2023-06-04 17:18:29 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  return runApp(const HelloWorld());
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(title: const Text('Hello World'),centerTitle: true,),
+         body: Center(
+           child: Container(
+             child: const Text('Welcome to Dart Programming Language',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.lightBlue
+                    ),
+             )    ,
+           ),
+         ),
+
+       ),
+    );
+  }
+}
